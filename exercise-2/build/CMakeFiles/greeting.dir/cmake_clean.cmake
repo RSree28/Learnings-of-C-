fc0989362f36d08bdf00ff86382d6d7618dc7644
Roja Sree Mallavarapu <roja.sree28@gmail.com>
2023-03-17 14:47:17 +0530

    initial setup

---
+++ exercise-2/build/CMakeFiles/greeting.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/greeting.dir/greeting.cpp.o"
+  "CMakeFiles/greeting.dir/greeting.cpp.o.d"
+  "libgreeting.dylib"
+  "libgreeting.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/greeting.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
